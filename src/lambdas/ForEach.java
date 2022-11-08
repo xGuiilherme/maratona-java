a8f49b83bbcfaf8033a4371655f5c34d68ec5028
xGuiilherme <guiroox2@gmail.com>
2022-11-08 11:59:32 -0300

    Fix

--- src/lambdas/ForEach.java
+++ src/lambdas/ForEach.java
@@ -15,14 +15,23 @@ public class ForEach {
             System.out.println(nome);
         }
 
-        // Tento apenas um único parameto não precisa colocar dentro de (), tento uma única sentença de código pode remover o par de chaves {}.
+        // Tendo apenas um único parameto não precisa colocar dentro de (), tendo uma única sentença de código pode remover o par de chaves {}.
         System.out.println("\nLambda #01...");
-        aprovados.forEach((nome) -> {
-            System.out.println(nome + "!!!");
-        });
+        aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 
-        // Passa uma referencia para essa função.
-        System.out.println("\nMethod Reference...");
+        // Passa uma referencia para função.
+        System.out.println("\nMethod Reference #01...");
+
+        System.out.println("\nLambda #02...");
+        aprovados.forEach(nome -> meuImprimir(nome));
         aprovados.forEach(System.out::println);
+
+        // Passa uma referencia para uma função que eu criei.
+        System.out.println("\nMethod Reference #02...");
+        aprovados.forEach(ForEach::meuImprimir);
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println("Oi! Meu nome é " + nome);
     }
 }
