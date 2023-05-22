50c41db5f5a0b00ee85b4f2eb175b7d7c9ef1767
xGuiilherme <guiroox2@gmail.com>
2023-05-22 14:22:02 -0300

    Feat: Criado exemplo para fazer filtro usando funções streams

---
+++ src/streams/filter/Aluno.java
@@ -0,0 +1,12 @@
+package streams.filter;
+
+public class Aluno {
+
+    final String nome;
+    final double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+}
