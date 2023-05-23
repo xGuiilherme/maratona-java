d972174ee15ef4645f78c48e309fdf90c6a5f8c1
xGuiilherme <guiroox2@gmail.com>
2023-05-23 14:00:35 -0300

    Feat: Criado exemplos de exceção sobre exceção

---
+++ src/excecao/Causa.java
@@ -0,0 +1,30 @@
+package excecao;
+
+public class Causa {
+
+    public static void main(String[] args) {
+        try {
+            metodoA(null);
+        } catch (IllegalArgumentException e) {
+            if (e.getCause() != null) {
+                System.out.println(e.getCause().getMessage());
+            }
+        }
+    }
+
+    static void metodoA(Aluno aluno) {
+        try {
+            metodoB(aluno);
+        } catch (Exception causa) {
+            throw new IllegalArgumentException(causa);
+        }
+    }
+
+    static void metodoB(Aluno aluno) {
+        if (aluno == null) {
+            throw new NullPointerException("Aluno está NULLOOOO");
+        }
+
+        System.out.println(aluno.nome);
+    }
+}
